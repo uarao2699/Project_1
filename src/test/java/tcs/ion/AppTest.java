4cfed2fe8028052d03af6b478b52657086c29602
uarao2699 <67856313+uarao2699@users.noreply.github.com>
2020-07-14 15:07:26 +0530

    Add files via upload

---
+++ src/test/java/tcs/ion/AppTest.java
@@ -0,0 +1,20 @@
+package tcs.ion;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
